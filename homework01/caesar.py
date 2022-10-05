112a20578bf91b367e460f3258644136e96d7f82
Satori-Tamaba <superdenzelo@gmail.com>
2022-10-05 16:27:22 +0300

    допилил

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -12,32 +12,24 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
 
-    shifr = ''
-    cifr = '1234567890 .,/;[]!"№;%:?*()_+'
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    leter_index_big = dict(zip(alphabet_big, range(len(alphabet_big))))
-    index_leter_big = dict(zip(range(len(alphabet_big)), alphabet_big))
-    leter_index = dict(zip(alphabet, range(len(alphabet))))
-    index_leter = dict(zip(range(len(alphabet)), alphabet))
-    for leter in plaintext:
-        if (leter.isalpha()):
-            if (leter.isupper()):
-                index_big = (leter_index_big[leter] + shift) % 26
-                shifered_leter_big = index_leter_big[index_big]
-                shifr += shifered_leter_big
-            else:
-                index = (leter_index[leter] + shift) % 26
-                shifered_leter = index_leter[index]
-                shifr += shifered_leter
-        else:
-            if (leter in cifr):
-                shifr += leter
 
+
+    shifr = ''
+    for let in plaintext:
+        if (let.isupper()):
+            new_let = chr(((ord(let) - 65 + shift) % 26) + 65)
+            shifr += new_let
+        elif (let.islower()):
+            new_let = chr(((ord(let) - 97 + shift) % 26) + 97)
+            shifr += new_let
+        else:
+            shifr += let
     return shifr
 
 
+
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+
     """
     Decrypts a ciphertext using a Caesar cipher.
 
@@ -51,25 +43,13 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     shifr = ''
-    cifr = '1234567890 .,/;[]!"№;%:?*()_+'
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    leter_index_big = dict(zip(alphabet_big, range(len(alphabet_big))))
-    index_leter_big = dict(zip(range(len(alphabet_big)), alphabet_big))
-    leter_index = dict(zip(alphabet, range(len(alphabet))))
-    index_leter = dict(zip(range(len(alphabet)), alphabet))
-    for leter in ciphertext:
-        if (leter.isalpha()):
-            if (leter.isupper()):
-                index_big = (leter_index_big[leter] - shift) % 26
-                shifered_leter_big = index_leter_big[index_big]
-                shifr += shifered_leter_big
-            else:
-                index = (leter_index[leter] - shift) % 26
-                shifered_leter = index_leter[index]
-                shifr += shifered_leter
+    for let in ciphertext:
+        if (let.isupper()):
+            new_let = chr(((ord(let) - 65 - shift) % 26) + 65)
+            shifr += new_let
+        elif (let.islower()):
+            new_let = chr(((ord(let) - 97 - shift) % 26) + 97)
+            shifr += new_let
         else:
-            if (leter in cifr):
-                shifr += leter
-
+            shifr += let
     return shifr
