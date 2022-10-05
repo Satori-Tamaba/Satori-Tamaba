414337cd12f8f7d8db13e7bbc3d093430a235b8f
Satori-Tamaba <61435333+Satori-Tamaba@users.noreply.github.com>
2022-10-05 16:44:45 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,29 +15,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     
-    shifr = ''
-    cifr = '1234567890 .,/;[]!"№;%:?*()_+'
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    leter_index_big = dict(zip(alphabet_big, range(len(alphabet_big))))
-    index_leter_big = dict(zip(range(len(alphabet_big)), alphabet_big))
-    leter_index = dict(zip(alphabet, range(len(alphabet))))
-    index_leter = dict(zip(range(len(alphabet)), alphabet))
-    for leter in plaintext:
-        if(leter.isalpha()):
-            if(leter.isupper()):
-                index_big = (leter_index_big[leter]+ shift) % 26
-                shifered_leter_big = index_leter_big[index_big]
-                shifr+=shifered_leter_big
-            else:
-                index = (leter_index[leter] + shift) % 26
-                shifered_leter = index_leter[index]
-                shifr += shifered_leter
-        else:
-            if(leter in cifr):
-                shifr+=leter
-
-    return shifr
+    
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
@@ -54,29 +32,9 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
   
-    shifr = ''
-    cifr = '1234567890 .,/;[]!"№;%:?*()_+'
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alphabet_big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    leter_index_big = dict(zip(alphabet_big, range(len(alphabet_big))))
-    index_leter_big = dict(zip(range(len(alphabet_big)), alphabet_big))
-    leter_index = dict(zip(alphabet, range(len(alphabet))))
-    index_leter = dict(zip(range(len(alphabet)), alphabet))
-    for leter in ciphertext:
-        if(leter.isalpha()):
-            if(leter.isupper()):
-                index_big = (leter_index_big[leter]- shift) % 26
-                shifered_leter_big = index_leter_big[index_big]
-                shifr+=shifered_leter_big
-            else:
-                index = (leter_index[leter] - shift) % 26
-                shifered_leter = index_leter[index]
-                shifr += shifered_leter
-        else:
-            if(leter in cifr):
-                shifr+=leter
+    
 
-    return shifr
+    
     
 
 
